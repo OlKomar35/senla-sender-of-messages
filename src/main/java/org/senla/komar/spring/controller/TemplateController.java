eb34090ebd4f61ca81e2af1fa7862ff25a176381
Olga Komar <komar_ol@mail.ru>
2024-08-27 10:54:57 +0300

    issue #8: added task-6

---
+++ src/main/java/org/senla/komar/spring/controller/TemplateController.java
@@ -0,0 +1,37 @@
+package org.senla.komar.spring.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.senla.komar.spring.enums.DeliveryChannel;
+import org.senla.komar.spring.enums.MessageType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Olga Komar
+ * <p>
+ * Created at 26.08.2024
+ */
+@RestController
+@RequestMapping("/templates")
+public class TemplateController {
+
+  @GetMapping("/message-types")
+  public List<String> getMessageTypes() {
+    List<String> list = Arrays.stream(MessageType.values())
+        .map(MessageType::getName)
+        .collect(Collectors.toList());
+    return list;
+  }
+
+  @GetMapping("/delivery-channels")
+  public List<String> getDeliveryChannels() {
+    List<String> list = Arrays.stream(DeliveryChannel.values())
+        .map(DeliveryChannel::getName)
+        .collect(Collectors.toList());
+    return list;
+  }
+}
